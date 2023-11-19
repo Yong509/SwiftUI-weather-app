fbd47ec92eb81c8cd542fca2a4ea50f0ea64fdb4
Kerkwit Supawattanakul <yongkerkwith@gmail.com>
2023-11-19 23:11:19 +0700

    Initial Commit

--- swift-weather-app/ContentView.swift
+++ swift-weather-app/ContentView.swift
@@ -1,24 +1,93 @@
-//
-//  ContentView.swift
-//  swift-weather-app
-//
-//  Created by Yong on 19/11/2566 BE.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient:Gradient(colors:[Color.blue,Color("lightBlue")]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                 )
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack{
+                Text("Bangkok, TH")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(spacing: 10){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("35 °C")
+                        .font(.system(size: 70,weight: .medium))
+                        .foregroundColor(.white)
+                }
+                .padding(.bottom, 40)
+                
+                HStack(spacing: 20){
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 32)
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "sun.max.fill",
+                                   temperature: 35)
+                    WeatherDayView(dayOfWeek: "THU",
+                                   imageName: "wind",
+                                   temperature: 33)
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "sunset.fill",
+                                   temperature: 35)
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "cloud.drizzle.fill",
+                                   temperature: 32)
+                    
+                }
+                Spacer()
+                
+                Button{
+                    print("tapped")
+                }label: {
+                    Text("Change Day Time")
+                        .frame(width: 280, height: 50)
+                        .background(Color.white)
+                        .font(.system(size: 20,weight: .bold))
+                        .cornerRadius(10)
+                }
+                Spacer()
+            }
         }
-        .padding()
+   
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .font(.system(size: 20, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text("\(temperature) °C")
+                .font(.system(size: 20,weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
 }
