b880a357f77adc089b85b599ce488a43fec0d6f1
Kerkwit Supawattanakul <yongkerkwith@gmail.com>
2023-11-19 23:01:46 +0700

    Initial Commit

---
+++ swift-weather-app/swift_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_weather_appApp.swift
+//  swift-weather-app
+//
+//  Created by Yong on 19/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct swift_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
